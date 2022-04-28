2acf4e7bb4863283208a1dcd7564d54e57076d08
2910Nandini <100186370+2910Nandini@users.noreply.github.com>
2022-04-28 10:40:42 -0700

    Add files via upload

---
+++ forstring.py
@@ -0,0 +1,7 @@
+#to print characters of a string
+
+s=input("enter a string")
+print(type(s))
+print(s)
+for i in s:
+    print(i)
---
+++ string.py
@@ -0,0 +1,8 @@
+#basic string
+
+a="Hello"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
