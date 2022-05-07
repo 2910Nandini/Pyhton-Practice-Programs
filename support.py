2d6dfcccc11b05e68300c7e3ffc716cb24a45707
2910Nandini <100186370+2910Nandini@users.noreply.github.com>
2022-05-07 10:57:33 -0700

    Add files via upload

---
+++ support.py
@@ -0,0 +1,12 @@
+#defining a simple function for division to be imported into another program
+
+def div(x,y):
+        return x/y
+
+a=int(input("enter value of a: "))
+b=int(input("enter value of b: "))
+c=div(a,b)
+
+print("a= ",a)
+print("b= ",b)
+print("c= ",c)
